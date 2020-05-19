60d3d3473061d2c94f1f55ee1b42f7e466f83735
Gabriel Fritz <gabrielfritzsluzala@gmail.com>
2020-05-19 15:25:57 -0300

    Restarting project

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Learning Representations for Time Series Clustering in a project::
-
-    import dtcr
--- dtcr/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Learning Representations for Time Series Clustering."""
-
-__author__ = """FRI72"""
-__email__ = 'gabrielfritzsluzala@gmail.com'
-__version__ = '0.1.0'
